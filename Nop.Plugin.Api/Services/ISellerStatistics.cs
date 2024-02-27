7a772c1cd4e3b422dc859378468250355d7a3b8a
gustavo-dgs <gustavo.dgs19@gmail.com>
2024-02-27 11:03:20 -0400

    intento 1 de estadisticas

--- Nop.Plugin.Api/Services/ISellerStatistics.cs
+++ Nop.Plugin.Api/Services/ISellerStatistics.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Nop.Plugin.Api.DTOs.Base;
+using Nop.Plugin.Api.DTOs.Statistics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +8,13 @@ using System.Threading.Tasks;
 
 namespace Nop.Plugin.Api.Services;
 
-public interface ISellerStatistics
+#nullable enable
+
+public interface ISellerStatisticsApiService
 {
+    Task<BaseSyncResponse> GetLastestUpdatedItems2Async(
+        IList<int>? idsInDb, DateTime? lastUpdateUtc, int sellerId
+    );
+
+    List<List<object?>> GetItemsCompressed(IList<SellerStatisticsDto> items);
 }
